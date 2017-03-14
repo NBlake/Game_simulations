c91c84b08e4ab0a8e1b10471ed081378fe1f51cb
NBlake <nathan.blake.15@ucl.ac.uk>
2017-03-14 12:12:03 +0000

    Update MC_Sim.m

--- MC_Sim.m
+++ MC_Sim.m
@@ -41,5 +41,5 @@ figure; bar3(Sets/N);title('Probability of sets');ylabel('Number of dice');xlabe
 
 %Tables
 array2table(round(H/N,2),'VariableNames', {'one','two','three','four','five','six','seven','eight','nine','ten'}, 'RowNames', {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'})
-array2table(round(Width/N,2),'VariableNames', {'one','two','three','four','five','six','seven','eight','nine','ten'}, 'RowNames', {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'})
-array2table(round(Sets/N,2),'VariableNames', {'one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty'}, 'RowNames', {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'})
+array2table(round(Width/N,2),'VariableNames', {'one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty'}, 'RowNames', {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'})
+array2table(round(Sets/N,2),'VariableNames', {'one','two','three','four','five','six','seven','eight','nine','ten'}, 'RowNames', {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'})
